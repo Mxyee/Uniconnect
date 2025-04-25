35358d6a65008c1a36e32d9a7b4e2da742f48a69
hua <hyhflorahua@gmail.com>
2025-04-25 23:48:37 +0100

    feat: notification function include test

--- app/views.py
+++ app/views.py
@@ -1,25 +1,27 @@
-from flask import render_template, redirect, url_for, flash, request, send_file, send_from_directory
+from flask import render_template, redirect, url_for, flash, request, send_file, send_from_directory, abort
 from app import app
-from app.models import User
+from app.models import User, Notification
 from app.forms import ChooseForm, LoginForm
 from flask_login import current_user, login_user, logout_user, login_required, fresh_login_required
 import sqlalchemy as sa
-from app import db
+from app import db, mail
 from urllib.parse import urlsplit
 import csv
 import io
 import datetime
+from app.notification import trigger_notification
+from flask_mail import Message
 
 
 @app.route("/")
 def home():
-    return render_template('templates/home.html', title="Home")
+    return render_template('home.html', title="Home")
 
 
 @app.route("/account")
 @login_required
 def account():
-    return render_template('templates/account.html', title="Account")
+    return render_template('account.html', title="Account")
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -38,7 +40,49 @@ def login():
         if not next_page or urlsplit(next_page).netloc != '':
             next_page = url_for('home')
         return redirect(next_page)
-    return render_template('templates/generic_form.html', title='Sign In', form=form)
+    return render_template('generic_form.html', title='Sign In', form=form)
+
+
+
+@app.route('/notifications')
+@login_required
+def notifications():
+    notifications = Notification.query.filter_by(user_id=current_user.id).order_by(Notification.timestamp.desc()).all()
+    return render_template('notifications.html', title="Notification", notifications=notifications)
+
+@app.route('/notification/<int:id>')
+@login_required
+def view_notification(id):
+    notification = Notification.query.get_or_404(id)
+    if notification.user_id != current_user.id:
+        abort(403)
+    notification.is_read = True
+    db.session.commit()
+    return render_template('view_notification.html',title="Notifications", notification=notification)
+
+
+# test
+@app.route("/test_notify")
+@login_required
+def test_notify():
+    trigger_notification("joined_activity", user_id=current_user.id, activity_title="Flask Club")
+    return "Test notify"
+
+
+# test
+@app.route('/test_email')
+def test_email():
+    try:
+        msg = Message(
+            subject="Test notification",
+            recipients=["test@gmail.com"],
+            body="This is a test message."
+        )
+        mail.send(msg)
+        return "Test email sent!"
+    except Exception as e:
+        return f"<h2>Email Error:</h2><pre>{str(e)}</pre>"
+
 
 @app.route('/logout')
 def logout():
