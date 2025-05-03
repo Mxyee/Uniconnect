09f231b4d6c07a46fd3bb2774344253e04db7bd0
hua <hyhflorahua@gmail.com>
2025-05-03 23:24:31 +0100

    feat: add context processor to inject unread notification count globally

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, current_user
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-# from app.models import Notification
 from flask_mail import Mail
 
 
@@ -21,14 +20,19 @@ mail = Mail(app)
 from app import views, models
 from app.debug_utils import reset_db
 
+from flask_login import current_user
+from app.models import Notification
+
+@app.context_processor
+def inject_unread_count():
+    if current_user.is_authenticated:
+        count = Notification.query.filter_by(user_id=current_user.id, is_read=False).count()
+    else:
+        count = 0
+    return dict(unread_count=count)
+
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, sa=sa, so=so, reset_db=reset_db)
 
-
-# @app.context_processor
-# def inject_unread_count():
-#     if current_user.is_authenticated:
-#         unread = Notification.query.filter_by(user_id=current_user.id, is_read=False).count()
-#         return dict(unread_count=unread)
-#     return dict(unread_count=0)
+from app import scheduled_reminders
